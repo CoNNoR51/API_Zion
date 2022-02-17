54260278f2116550f3f3d575e7bfbe0f4c323d0f
Danila Matveev <danila.matveev.2019@gmail.com>
2022-02-17 18:04:47 +0300

    adding mininet to API

--- test.py
+++ test.py
@@ -9,8 +9,7 @@ if __name__ == '__main__':
 
     requests.post("http://127.0.0.1:5000/set_loss_and_delay/", data={'id': convertedDict['id'],
                                                                      'loss': '12', 'delay': '33'})
-    # requests.post("http://127.0.0.1:5000/set_loss_and_delay/", data={'id': '45826874156537428596',
-    #                                                                  'loss': '77', 'delay': '89'})
+
     w = requests.get('http://127.0.0.1:5000/get_bw/', data={'id': convertedDict['id']})
 
     convertedDict1 = json.loads(w.text)
